8bd46798274a7afa11302fc6331338b4dd0cf0ca
Olive <tvvaisakhtv@gmail.com>
2026-08-22 14:49:14 +0530

    add ui to select

--- cogs/search_anime.py
+++ cogs/search_anime.py
@@ -15,47 +15,47 @@ class Anime(commands.Cog):
     async def search(self, interaction: discord.Interaction, anime: str):
         await interaction.response.defer()
         ani_data = AS.search_anime(anime)
-        embed = discord.Embed(
-            title=f"Search results for {anime}",
-            color=discord.Color.green()
-        )
-        for i, item in enumerate(ani_data, start=1):
-            embed.add_field(
-                name=f"{i}. {item['title']}",
-                value=(
-                    f"Status: {item.get('status', 'Unknown')}\n"
-                    
-                    f"ID: {item.get('id')}\n"
-                    f"Episodes: {item.get('episodes', 'Unknown')}\n"
-
-                ),
-                inline=False
-            )
-
-        await interaction.followup.send(embed=embed)
-
-    @app_commands.command(name="get_schedule", description="Get schedule")
-    @app_commands.describe(id="id of the anime to get schedule")
-    async def get_schedule(self, interaction: discord.Interaction, id: int):
-        await interaction.response.defer()
-        ani_schedule = AS.get_schedule(id)
-        embed = discord.Embed(
-            title=f"Schedule for {ani_schedule['Media']['title']['english']}",
-            description=f"Next Episode: {ani_schedule['Media']['nextAiringEpisode']['episode']}\n"
-                        f"Airing At: {datetime.fromtimestamp(ani_schedule['Media']['nextAiringEpisode']['airingAt']).strftime("%B %d, %Y - %I:%M %p")}",
-            color=discord.Color.red()
-        )
-        for item in ani_schedule['Media']['airingSchedule']['nodes']:
-            embed.add_field(
-                name=f"Episode: {item['episode']}",
-                value=(
-                    f"{datetime.fromtimestamp(item['airingAt']).strftime("%B %d, %Y - %I:%M %p")}"
-                ),
-                inline=False
-            )
-
-        await interaction.followup.send(embed=embed)
+        if not len(ani_data) == 0:
+            await interaction.followup.send(view=AnimeView(result=ani_data))
+        else:
+            await interaction.followup.send("No anime found. :(")
 
 
 async def setup(bot):
     await bot.add_cog(Anime(bot))
+
+
+
+
+class AnimeButton(discord.ui.Button):
+    def __init__(self, anime_id, title,status, row):
+        if status == "RELEASING":
+            button_type=discord.ButtonStyle.success
+        else:
+            button_type=discord.ButtonStyle.secondary
+        super().__init__(label=title, style=button_type, row=row)
+        self.anime_id = anime_id
+
+    async def callback(self, interaction: discord.Interaction):
+        ani_schedule = AS.get_schedule(self.anime_id)
+        embed = discord.Embed(
+            title=f"Schedule for {ani_schedule['Media']['title']['english']}",
+            description=f"Next Episode: **{ani_schedule['Media']['nextAiringEpisode']['episode']}**\n"
+                        f"Airing At: **{datetime.fromtimestamp(ani_schedule['Media']['nextAiringEpisode']['airingAt']).strftime('%B %d - %I:%M %p')}**",
+            color=discord.Color.red()
+        )
+        for item in ani_schedule['Media']['airingSchedule']['nodes']:
+            embed.add_field(
+                name=f"Episode: {item['episode']} - {datetime.fromtimestamp(item['airingAt']).strftime('%B %d')}\n",
+                value="\n",
+                inline=False
+            )
+        await interaction.response.edit_message(embed=embed,view=None)
+
+
+class AnimeView(discord.ui.View):
+    def __init__(self, result):
+        super().__init__()
+        for index ,ani in enumerate(result):
+            self.add_item(AnimeButton(ani['id'], ani['title'], status=ani.get('status', 'Unknown'), row=index))
+
